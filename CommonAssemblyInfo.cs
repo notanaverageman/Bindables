f1503b76f0e10b00bfa6ff98bb67f291ef28dd90
yusuf-gunaydin <yusuf_tarik@hotmail.com>
2016-11-16 21:34:09 +0200

    Added a project for nuget packaging.

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Bindables")]
+[assembly: AssemblyProduct("Bindables")]
+[assembly: AssemblyCompany("Yusuf Tarık Günaydın")]
+[assembly: AssemblyDescription("Fody add-in for creating dependency properties from auto properties.")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
