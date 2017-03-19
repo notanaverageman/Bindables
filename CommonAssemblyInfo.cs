f66fbf5862f0a3c226766f63a24ffa739933b04b
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2017-03-19 19:21:32 +0300

    Increment version.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("Bindables")]
 [assembly: AssemblyCompany("Yusuf Tarık Günaydın")]
 [assembly: AssemblyDescription("Fody add-in for creating dependency and attached properties from auto properties.")]
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
