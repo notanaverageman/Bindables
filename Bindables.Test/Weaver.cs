bb4d445199948fa49870fa6a452fb480e5618f3e
Yusuf Tarık Günaydın <yusuf.gunaydin@sestek.com>
2019-09-18 08:26:47 +0300

    Add .NET Core support.

--- Bindables.Test/Weaver.cs
+++ Bindables.Test/Weaver.cs
@@ -1,10 +1,13 @@
 using System;
-using System.CodeDom.Compiler;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using Bindables.Fody;
-using Microsoft.CodeDom.Providers.DotNetCompilerPlatform;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
 using Mono.Cecil;
 using NUnit.Framework;
 
@@ -16,68 +19,70 @@ namespace Bindables.Test
 	{
 		public static Assembly Weave(string code)
 		{
-			string assemblyName = Guid.NewGuid() + ".dll";
+			SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
 
-			CompilerParameters parameters = new CompilerParameters
+			string[] assemblyPaths =
 			{
-				GenerateExecutable = false,
-				OutputAssembly = assemblyName,
-				
+				typeof(Exception).Assembly.Location,
+				typeof(DependencyObject).Assembly.Location,
+				typeof(DependencyPropertyAttribute).Assembly.Location,
+				typeof(FrameworkPropertyMetadataOptions).Assembly.Location,
+				typeof(Console).GetTypeInfo().Assembly.Location,
+				Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Runtime.dll")
 			};
 
-			parameters.ReferencedAssemblies.Add(typeof(Exception).Assembly.Location);
-			parameters.ReferencedAssemblies.Add(typeof(DependencyObject).Assembly.Location);
-			parameters.ReferencedAssemblies.Add(typeof(DependencyPropertyAttribute).Assembly.Location);
-			parameters.ReferencedAssemblies.Add(typeof(FrameworkPropertyMetadataOptions).Assembly.Location);
+			PortableExecutableReference[] references = assemblyPaths.Select(x => MetadataReference.CreateFromFile(x)).ToArray();
 
-			CodeDomProvider codeProvider = CreateCodeProvider();
-
-			CompilerResults compilerResults = codeProvider.CompileAssemblyFromSource(parameters, code);
-
-			Assert.IsEmpty(compilerResults.Errors);
+			CSharpCompilation compilation = CSharpCompilation.Create(
+				Guid.NewGuid() + ".dll",
+				new[] { syntaxTree },
+				references,
+				new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
 			using (MemoryStream stream = new MemoryStream())
-			using (ModuleDefinition module = ModuleDefinition.ReadModule(assemblyName, new ReaderParameters { InMemory = true }))
 			{
-				File.Delete(assemblyName);
+				EmitResult result = compilation.Emit(stream);
 
-				ModuleWeaver weavingTask = new ModuleWeaver
+				if (!result.Success)
 				{
-					ModuleDefinition = module
-				};
+					Console.Error.WriteLine("Compilation failed!");
 
-				weavingTask.Execute();
-				module.Write(stream);
-				stream.Seek(0, SeekOrigin.Begin);
+					IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+						diagnostic.IsWarningAsError ||
+						diagnostic.Severity == DiagnosticSeverity.Error);
 
-				using (FileStream fileStream = File.Create($"{Guid.NewGuid()}.dll"))
-				{
-					stream.CopyToAsync(fileStream);
-					stream.Seek(0, SeekOrigin.Begin);
+					foreach (Diagnostic diagnostic in failures)
+					{
+						Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+					}
+
+					Assert.That(result.Success);
 				}
 
-				return Assembly.Load(stream.ToArray());
+				stream.Seek(0, SeekOrigin.Begin);
+
+				using (MemoryStream conversionStream = new MemoryStream())
+				using (ModuleDefinition module = ModuleDefinition.ReadModule(stream, new ReaderParameters { InMemory = true }))
+				{
+
+					ModuleWeaver weavingTask = new ModuleWeaver
+					{
+						ModuleDefinition = module
+					};
+
+					weavingTask.Execute();
+					module.Write(conversionStream);
+					conversionStream.Seek(0, SeekOrigin.Begin);
+
+					using (FileStream fileStream = File.Create($"{Guid.NewGuid()}.dll"))
+					{
+						conversionStream.CopyToAsync(fileStream);
+						conversionStream.Seek(0, SeekOrigin.Begin);
+					}
+
+					return Assembly.Load(conversionStream.ToArray());
+				}
 			}
-		}
-
-		private static CodeDomProvider CreateCodeProvider()
-		{
-			CSharpCodeProvider codeProvider = new CSharpCodeProvider();
-
-			// Fix the roslyn folder path.
-			object settings =
-				codeProvider.GetType()
-					.GetField("_compilerSettings", BindingFlags.Instance | BindingFlags.NonPublic)
-					.GetValue(codeProvider);
-
-			string path =
-				settings.GetType().GetField("_compilerFullPath", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(settings) as
-					string;
-
-			settings.GetType()
-				.GetField("_compilerFullPath", BindingFlags.Instance | BindingFlags.NonPublic)
-				.SetValue(settings, path.Replace(@"bin\roslyn\", @"roslyn\"));
-			return codeProvider;
 		}
 	}
 }
